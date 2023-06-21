d4cdbe05f16f46e767ebe47063b9cb5c4d911c31
NicoAchard <achardn@gmail.com>
2023-06-21 09:53:01 -0300

    back login

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,18 +9,19 @@ async function tokens(req, res) {
     });
 
     if (!user) {
-      return res.json({ response: "Credenciales inválidas" });
+      return res.json({ response: "Credenciales inválidas", status: 404 });
     } else {
       console.log(user);
       const isMatch = await user.comparePassword(req.body.password);
       console.log(isMatch);
       if (!isMatch) {
-        return res.json({ response: "Credenciales inválidas" });
+        return res.json({ response: "Credenciales inválidas", status: 404 });
       } else {
         const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET);
         return res.json({
           token,
           data: user,
+          status: 200,
         });
       }
     }
