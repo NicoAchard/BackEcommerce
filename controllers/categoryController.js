13857ac163f55bb9a66ab4c34bf74b6c81623ade
IvanPintos <ProyectosIvanPintos@gmail.com>
2023-06-22 12:10:32 -0300

    Categories changes

---
+++ controllers/categoryController.js
@@ -0,0 +1,47 @@
+const { Category } = require("../models");
+
+async function index(req, res) {
+  try {
+    const categories = await Category.findAll();
+    return res.json(categories);
+  } catch (e) {
+    console.log(error);
+    return res.json({ response: "Something went wrong. Please try again later", status: 400 });
+  }
+}
+
+async function show(req, res) {}
+
+async function store(req, res) {
+  try {
+    const { name, description } = req.body;
+    const category = Category.create({
+      name,
+      description,
+    });
+    return res.json({ response: "The category was created successfully", status: 200 });
+  } catch (error) {
+    console.log(error);
+    return res.json({ response: "Something went wrong. Please try again later", status: 400 });
+  }
+}
+
+async function update(req, res) {}
+
+async function destroy(req, res) {
+  try {
+    await Category.destroy({ where: { id: req.params.id } });
+    return res.json({ response: "The category was deleted successfully", status: 200 });
+  } catch (error) {
+    console.log(error);
+    return res.json({ response: "Something went wrong. Please try again later", status: 400 });
+  }
+}
+
+module.exports = {
+  index,
+  show,
+  store,
+  update,
+  destroy,
+};
