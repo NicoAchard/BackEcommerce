0a5052306ab4e853560b8a05a1523e47477c7921
IvanPintos <ProyectosIvanPintos@gmail.com>
2023-06-28 15:23:00 -0300

    test deploy

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+router.post("/", (req, res) => {
+  return res.json({ response: "Exito" });
+});
 router.post("/tokens", authController.tokens);
 
 module.exports = router;
